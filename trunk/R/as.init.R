39ea473c44c473c7a9324f9103005dd19b666ca4
bergsmat <bergsmat@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2012-05-03 21:09:47 +0000

    support for init and initList
    git-svn-id: svn://scm.r-forge.r-project.org/svnroot/metrumrg@134 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- trunk/R/as.init.R
+++ trunk/R/as.init.R
@@ -7,8 +7,9 @@
 # Need to support get/set fixed status.
 
 as.init <- function(x,...)UseMethod('as.init')
+as.init.init <- function(x=numeric(0),fixed=FALSE,comment=character(0),...)as.init.numeric(x=x,fixed=fixed,comment=comment,...)
 as.init.numeric <- function(x=numeric(0),fixed=FALSE,comment=character(0),...){
-	#x may be one,two, or three values
+    #x may be one,two, or three values
 	#init, low/init, or low/init/up
 	stopifnot(length(x)<=3,is.logical(fixed),inherits(comment,'character'))
 	y <- c(-Inf,NA,Inf)
@@ -70,76 +71,22 @@ fixed.init <- function(x,...)!any(is.na(x)) & length(unique(x)) == 1
 }
 
 as.initList <- function(x,...)UseMethod('as.initList')
-as.initList.list <- function(x,...){
-  stopifnot(length(x)>0)
+as.initList.list <- function(x,comment=character(0),...){
+  stopifnot(length(x)>0,is.character(comment))
   is.init <- sapply(x,inherits,'init')
   class(x) <- c('initList',class(x))
+  if(length(comment)) comment(x) <- comment
   x
 }
-as.character.initList <- function(x,...)c('',unlist(lapply(x,as.character)))
+as.character.initList <- function(x,...){
+  com <- comment(x)
+  if(is.null(com))com <- ''
+  else com <- c('',paste(';',com))
+  y <- c(com,unlist(lapply(x,as.character)))
+  y
+}
 format.initList <-function(x,...)as.character(x,...)
 print.initList <-function(x,...)print(format(x,...))
-
-as.initList.character <- function(x,...){
-  #each init starts on a particular line.  The comment on that same line, and
-  #any pure comment to follow, will accompany.
-  x <- sub('^[ \t]+','',x) #no leading whitespace
-  x <- sub('[ \t]+$','',x) #no trailing whitespace
-  x <- x[x!=''] #no blank lines
-  dat <- cumsum(grepl('^[^;]',x))#test for leading nonsemicolon
-  x <- x[dat>0] #discard lines before first data line
-  commentIndex <- x %contains% ';' #
-  dataIndex <- !grepl('^[;]',x) #first char is not semicolon
-  comments <- .comments(x)#comments only
-  data <- sub(';.*$','',x) # data only
-  #data <- data[data!=''] # just data lines
-  #comments <- split(comments,cumsum(dataIndex)) # number comment chunks like data lines
-  if(length(data)==0)stop('no uncommented data to parse')
-  data <- paste(data,collapse='\n')
-  data <- gsub('[ \t]+',' ',data) #single space delimiters
-  data <- gsub('FIXED','FIX',data)
-  data <- gsub(' FIX','FIX',data) #only remaining spaces should be internal or external delimiters
-  data <- gsub('INF','Inf',data)
-  data <- unlist(strsplit(data,'(',fixed=TRUE))
-  data <- unlist(.explicit(data))
-  data <- unlist(strsplit(data,' '))
-  data <- data[data!='']
-  #data <- data[data!='\n']
-  fixed <- data %contains% 'FIX'  
-  data <- gsub('FIX','',data)
-  line <- seq_along(dataIndex)
-  line[!dataIndex] <- NA
-  line <- locf(line)
-  names(comments) <- line
-  comments <- comments[comments!='']
-  #line <- cumsum(data %contains% '\n')
-  #data <- gsub('\n','',data)
-  data <- strsplit(data,',')
-  numreturns <- lapply(data,function(x)gregexpr('\n',x))
-  numreturns <- lapply(numreturns, function(x)lapply(x,function(y)y>=0))
-  numreturns <- lapply(numreturns, unlist)
-  numreturns <- lapply(numreturns,sum)
-  numreturns <- unlist(numreturns)
-  cumreturns <- cumsum(numreturns)
-  lastWindow <- cumreturns + 1
-  ranges <- lapply(seq_along(numreturns),function(slot)seq(from=numreturns[[slot]],to=lastWindow[[slot]]))
-  data <- lapply(data,as.numeric)
-  stopifnot(length(data) == length(fixed))
-  inits <- lapply(
-    seq_along(data),
-    function(i){
-      d <- data[[i]]
-      com <- as.character(comments[names(comments) %in% ranges[[i]]])
-      f <- fixed[[i]]
-      out <- as.init(x=d,fixed=f,comment=com)
-      if(all(is.na(d)))out <- NULL
-      out
-    }
-  )
-  inits <- inits[!sapply(inits,is.null)]
-  as.initList(inits)
-}
-
 .comments <- function(x)sub('^[^;]*;?(.*)$','\\1',x)
 .space2comma <- function(x){
   if(!length(x))return(x)
@@ -181,3 +128,132 @@ fixed.initList <- function(x,...)sapply(x,fixed)
 	for(i in seq_along(value))fixed(x[[i]]) <- value[[i]]
 	x
 }
+.initcomments <- function(x)sub('^[^;]*;?(.*)$','\\1',x)
+.initdata <- function(x)sub(';.*','',x)
+.initEstimateNum <- function(x){
+  # an estimate is initiated by a parenthesis, or by a qualifying character.
+  # a qualifying character is a numeric or reserved char.
+  # if initiated by parenthesis, it is closed by parenthesis, else by next
+  # qualifying character
+  # only 0123456789.-+eI( can initiate an estimate. For (, only ) can close it. For the others,
+  # the first space or newline ends the numeric region.
+  y <- numeric(0)
+  openers <- c('0','1','2','3','4','5','6','7','8','9','.','-','+','(')
+  closers <- c(' ','\n','\t') #or end of vector
+  region <- 0
+  parenthetical <- FALSE
+  active <- FALSE
+  for(i in x){
+    if(!active){
+      if(i %in% openers){
+        region <- region + 1
+        active <- TRUE
+      }
+    }else{
+      if(parenthetical){
+        if(i==')') active <- FALSE
+      }else{
+        if(i %in% closers) active <- FALSE
+      }
+    }
+    if(i=='(')parenthetical <- TRUE
+    if(i==')')parenthetical <- FALSE
+    y <- append(y,region)
+  }
+  y
+}
+
+.initLineNum <- function(x){
+  newline <- x=='\n'
+  #I am the first character in a line if I follow a newline, or if I am the veryfirst character.
+  frst <- prev(newline)
+  frst[is.na(frst)] <- TRUE
+  linenum <- cumsum(frst)
+  linenum
+}
+as.initList.character <- function(x,...){
+  stopifnot(length(x)>0)
+  comments <- .initcomments(x)
+  data <- .initdata(x)
+  data <- paste(data,collapse='\n')
+  z <- strsplit(data,NULL)[[1]]
+  #classify each character in z as belonging to a particular line and particular estimate.
+  est <- .initEstimateNum(z)
+  line <- .initLineNum(z)
+  inits <- split(z,est)
+  inits <- lapply(inits,paste,collapse='')
+  globalcom <- comments[unique(line[est==0])]
+  if(0 %in% est) inits <- inits[-1]
+  inits <- lapply(
+    seq_along(inits),
+    function(i){
+      x <- inits[[i]]
+      com <- comments[.initRelComment(i,est,line)]
+      com <- com[com!='']
+      comment(x) <- com
+      x
+    }
+  )
+  inits <- lapply(
+    seq_along(inits),
+    function(i){
+      com <- comment(inits[[i]])
+      if(is.null(com))com <- character(0)
+      out <- .as.init.character(inits[[i]],comment=com)
+      out
+    }
+  )
+  as.initList(inits,comment=globalcom)
+}
+.initRelComment <- function(i,est,line){
+  #need numbers for comment lines for any comment on my line, plus any trailing
+  #comments, i.e. those after this line but before a new est is started
+  #use comment groups: where runhead est coincides with runhead line
+  stopifnot(length(est)==length(line))
+  groupstart <- runhead(est) & runhead(line)
+  group <- cumsum(groupstart)
+  mygroup <- unique(group[est==i])#probably only 1
+  mylines <- unique(line[group==mygroup])
+  mylines
+}
+.as.init.character <- function(x,comment=character(0),...){#limited utility
+  stopifnot(length(x)==1)
+  fixed = FALSE
+  if(x %contains% 'FIX|FIXED') fixed <- TRUE
+  x <- gsub('FIXED','',x)
+  x <- gsub('FIX','',x)
+  x <- gsub('\n','',x)
+  x <- sub('(','',x,fixed=TRUE)
+  x <- sub(')','',x,fixed=TRUE)
+  x <- gsub(' +',' ',x)
+  x <- sub('^ ','',x)
+  x <- sub(' $','',x)
+  x <- gsub(',? ',',',x)
+  x <- strsplit(x,',')[[1]]
+  x <- as.numeric(x)
+  x <- as.init(x,fixed=fixed,comment=comment,...)
+  x
+}
+  
+  
+ 
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
