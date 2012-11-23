918371f10169cb624fb087b4aaaaa17f22951f33
bergsmat <bergsmat@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2012-11-23 18:15:49 +0000

    5.26
    git-svn-id: svn://scm.r-forge.r-project.org/svnroot/metrumrg@218 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- trunk/R/PLOTR.R
+++ trunk/R/PLOTR.R
@@ -278,7 +278,7 @@ dataFormat <- function(
 	run,
 	...
 ){
-    if (logtrans) tabdata <- backtrans(tabdata,intersect(names(tabdata),c('DV','PRED','IPRE','IPRED')))    
+    if (logtrans) tabdata <- backtrans(tabdata,intersect(names(tabdata),c('DV','PRED','NPRED','CPRED','CPREDI','EPRED','IPRE','IPRED')))    
     available <- unique(c(names(tabdata),names(covdata),names(pardata)))
     grp <- strain(grp,available)
     cont.cov <- strain(cont.cov,available)
