97be428fcbefe282c47f5f6cab59e4fcae61fffc
bergsmat <bergsmat@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2013-03-15 20:14:17 +0000

    bugfix
    git-svn-id: svn://scm.r-forge.r-project.org/svnroot/metrumrg@269 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- trunk/R/timeline.R
+++ trunk/R/timeline.R
@@ -2,8 +2,8 @@
   if(missing(e2))return(e1)
   if(inherits(e1,'timepoint') & inherits(e2,'timepoint'))stop('addition is undefined for two timepoints')
   as <- class(e1)[[1]]
-  coerce <- match.fun(glue('as.',as))
   if(inherits(e2,'timepoint')) as <- class(e2)
+  coerce <- match.fun(glue('as.',as))
   if(inherits(e1,'duration') & inherits(e2,'duration') & !identical(class(e1)[[1]],class(e2)[[1]]))message('coercing to class ',class(e1)[[1]])
   e1 <- as.numeric(as.second(e1))
   e2 <- as.numeric(as.second(e2))
