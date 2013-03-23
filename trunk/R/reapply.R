1a4b637751afbcf5c49a87a7ee9a9bb5907dcb3d
bergsmat <bergsmat@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2013-03-23 00:56:49 +0000

    5.35 candidate
    git-svn-id: svn://scm.r-forge.r-project.org/svnroot/metrumrg@283 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- trunk/R/reapply.R
+++ trunk/R/reapply.R
@@ -1,40 +1,20 @@
 reapply <- 
-function (x, INDEX, FUN, where, ...) 
-{
-	if(missing(where))where <- rep(TRUE,length(x))
-	where <- rep(where,length.out=length(x))
-	x[!where] <- NA
-	if(!is.list(INDEX)) INDEX <- list(INDEX)
-	INDEX <- lapply(INDEX,function(x)as.integer(factor(x)))
-	INDEX <- as.integer(do.call(interaction,c(INDEX,drop=TRUE)))
-	form <- tapply(x, INDEX)
-	calc <- tapply(x, INDEX, FUN, ..., simplify=FALSE)
-	need <- table(form)
-	calc <- lapply(
-		seq_along(calc),
-		function(cell)rep(
-			calc[[cell]],
-			length.out=need[[
-				as.character(cell)
-			]]
-		)
-	)
-	calc <- c(calc,list(rep(NA,sum(is.na(form)))))
-	form[is.na(form)] <- length(calc)
-	grps <- split(form,form)
-	grps <- lapply(
-		grps, 
-		seq_along
-	)
-	elem <- unsplit(grps,form)
-	sapply(
-		seq_along(form),
-		function(i)calc[[
-			form[[i]]
-		]][[
-			elem[[i]]
-		]]
-	)
-}
-
-
+  function (x, INDEX, FUN, where, ...) 
+  {
+    if(missing(where))where <- rep(TRUE,length(x))
+    where <- rep(where,length.out=length(x))
+    where[is.na(where)] <- TRUE
+    where <- factor(where,levels=c(FALSE,TRUE))
+    if(!is.list(INDEX)) INDEX <- list(INDEX)
+    INDEX <- do.call(interaction,c(INDEX,list(drop=TRUE)))
+    WINDEX <- interaction(where,INDEX,drop=FALSE)
+    wvals <- split(x,WINDEX)
+    vals <- wvals[c(FALSE,TRUE)]
+    stopifnot(identical(length(vals),length(levels(INDEX))))
+    val <- lapply(vals, FUN, ...)
+    dim <- table(INDEX)
+    val <- lapply(seq_along(val),function(i)rep(val[[i]], length.out=dim[[i]]))
+    split(x,INDEX) <- val
+    x[is.na(INDEX)] <- NA
+    x
+  }
