99e834bad75450ac004869ab4b9ea421a824a913
danp <danp@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2014-12-18 14:33:58 +0000

    blow away old version of metrumrg
    git-svn-id: svn://scm.r-forge.r-project.org/svnroot/metrumrg@443 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/map.R
+++
@@ -1,6 +0,0 @@
-map <-
-function(x,from,to){
-	stopifnot(length(to)==length(from))
-	to[match(x,table=from)]
-}
-
--- pkg/R/shuffle.R
+++
@@ -1,5 +0,0 @@
-shuffle <- function(x,who,after=NA){
-	nms <- names(x)[!names(x) %in% who]
-	nms <- append(nms, who, after=match(after,nms,nomatch=0))
-	x[nms]
-}
